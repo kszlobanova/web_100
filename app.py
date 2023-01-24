00d3272278d9252818b73ebf19489039ecbba7e6
KszLobanova <kszlobanova@gmail.com>
2023-01-24 21:29:30 +0300

    edited python

--- app.py
+++ app.py
@@ -10,6 +10,11 @@ def index():
     # return content
     return render_template('index.html')
 
-@app.route('/about.html')
-def about():
-    return render_template('about.html')
+@app.route('/about_rus.html')
+def about_rus():
+    return render_template('about_rus.html')
+
+
+@app.route('/about_eng.html')
+def about_eng():
+    return render_template('about_eng.html')

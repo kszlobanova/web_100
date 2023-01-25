973631fabf1c648a8531294be5b9032e7e8507cf
KszLobanova <kszlobanova@gmail.com>
2023-01-25 11:11:05 +0300

    deleted header and footer from index.html

--- app.py
+++ app.py
@@ -3,11 +3,9 @@ from flask import render_template, Flask, request
 app = Flask(__name__)
 
 @app.route('/')
+
 @app.route('/index.html')
 def index():
-    # with open('index.html', 'r') as f:
-    #     content = f.read()
-    # return content
     return render_template('index.html')
 
 @app.route('/about_rus.html')
